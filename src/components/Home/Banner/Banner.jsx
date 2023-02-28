3ac0d9dd1c6df6e2315e0aa3b04552f51a39ff2c
abmo475 <abmo475@gmail.com>
2023-02-28 15:09:25 +0300

    banner chnages

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -9,10 +9,7 @@ import fashionSale2 from '../../../assets/images/Banners/fashion-sale.png';
 import gadgetSale from '../../../assets/images/Banners/gadget-sale.jpg';
 import kitchenSale from '../../../assets/images/Banners/kitchen-sale.png';
 
-// import poco from '../../../assets/images/Banners/poco-m4-pro.webp';
-// import realme from '../../../assets/images/Banners/realme-9-pro.webp';
-// import oppo from '../../../assets/images/Banners/oppo-reno7.webp';
-
+ 
 export const PreviousBtn = ({ className, onClick }) => {   
   return (
     <div className={className} onClick={onClick}>
